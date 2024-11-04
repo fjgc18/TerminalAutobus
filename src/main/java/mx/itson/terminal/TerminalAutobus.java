6d6751d8808585c728b8eb80331ecea315a6ad22
Thinkpad <Thinkpad@192.168.1.69>
2024-11-03 17:59:48 -0700

    primer commit

---
+++ src/main/java/mx/itson/terminal/TerminalAutobus.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package mx.itson.terminal;
+
+/**
+ *
+ * @author Thinkpad
+ */
+public class TerminalAutobus {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
